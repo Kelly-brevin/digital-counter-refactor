32677aaea8017f2d7eb24ae1638134951b0834d1
kelly brevin <kellybrevin83@gmail.com>
2025-11-29 12:49:07 +0300

    classes

---
+++ script.js
@@ -0,0 +1,13 @@
+//wire up the buttons
+const displayEl = document.getElementById("count");
+
+const subtractBtn = document.getElementById("subtract");
+
+document.getElementById("add").addEventListener("click,()=>{}");
+document.getElementById("subtract").addEventListener("click,()=>{}");
+document.getElementById("reset");
+addEventListener("click,()=>{}");
+
+class Counter {
+  constructor() {}
+}
