fe1882b9da26f4bc5717d69108e1c0b252f49edd
kelly brevin <kellybrevin83@gmail.com>
2025-11-29 13:10:20 +0300

    new commits

--- script.js
+++ script.js
@@ -9,5 +9,28 @@ document.getElementById("reset");
 addEventListener("click,()=>{}");
 
 class Counter {
-  constructor() {}
+  constructor(displayEl, subtractBtn) {
+    this.count = 0;
+    this.displayEl = displayEl;
+    this.subtractBtn = subtractBtn;
+    this.updateUI();
+  }
+  increment() {
+    this.count++;
+    this.updateUI();
+  }
+  decrement() {
+    if (this.count > 0) {
+      this.count--;
+    }
+    this.updateUI();
+  }
+  reset() {
+    this.count = 0;
+    this.updateUI();
+  }
+  updateUI() {
+    this.displayEl.textContent = this.count;
+    this.subtractBtn.disabled = this.count === 0;
+  }
 }
